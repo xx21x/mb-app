6b6383bcd9e35454a658f1d5753514057e53878f
Bill Xiong <billxiong@hotmail.com>
2021-08-01 12:22:35 +0800

    initial commit

---
+++ posts/admin.py
@@ -0,0 +1,8 @@
+#posts/admin.py
+
+from django.contrib import admin
+
+# Register your models here.
+from .models import Post
+
+admin.site.register(Post)
---
+++ posts/models.py
@@ -0,0 +1,9 @@
+#posts/models.py
+
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    text = models.TextField()
+    def __str__(self):
+        return self.text[:50]
